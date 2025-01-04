dad8931f9341831a1c9093df8ef92e218497cd32
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-04 22:56:38 +0300

    First commit for the angular tutorial. Contains information about how the component bundle works. Interaction between ts file and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-project';
+  title: string = 'my-project';
+  isVisible: boolean = false;
+  items: string[] = ['Элемент 1', 'Элемент 2', 'Элемент 3'];
+  newItem: string = ''; // Добавлено свойство для двусторонней привязки
+  /**
+   * Метод для переключения видимости списка
+   */
+  public toggleVisibility(): void {
+    this.isVisible = !this.isVisible;
+  };
+  /**
+   * Метод для добавления нового элемента в список
+   */
+  public addItem(): void {
+    if (this.newItem.trim()) { // Проверка на пустую строку
+      this.items.push(this.newItem.trim());
+      this.newItem = ''; // Очистка поля ввода после добавления
+    }
+  };
 }
