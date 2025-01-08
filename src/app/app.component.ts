83ec50ca30d46a79607fdf4a6e73326415c75405
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-08 22:12:09 +0300

    example of how map works from rxJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
-import {JsonPipe} from "@angular/common";
+import { AsyncPipe, JsonPipe, NgSwitch, NgSwitchCase, NgSwitchDefault } from "@angular/common";
+import { map, Observable, of } from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, JsonPipe],
+  imports: [RouterOutlet, FormsModule, JsonPipe, NgSwitch, NgSwitchCase, NgSwitchDefault, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   dataValue = signal(0);
+  stream$!: Observable<number>;
 
   public changeDataValue(event: Event): void {
     const input = event.target as HTMLInputElement;
     const value = Number(input.value);
     this.dataValue.set(value);
+    this.stream$ = of(this.dataValue()).pipe(
+      map(value => value * 2) // Умножаем значение на два
+    );
+  }
+
+  ngOnInit() {
+    // Инициализация stream$ при загрузке компонента, если необходимо
+    this.stream$ = of(this.dataValue()).pipe(
+      map(value => value * 2)
+    );
   }
 }
