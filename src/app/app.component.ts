8784c4f55b5e7d8ae2e059628442b743b4aeaed5
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-08 21:28:11 +0300

    example with directive switch case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
-import { Observable, Subscription, fromEvent } from "rxjs";
 import {JsonPipe} from "@angular/common";
 
 @Component({
@@ -11,43 +10,12 @@ import {JsonPipe} from "@angular/common";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
-  subscription: Subscription | null = null;
+export class AppComponent{
+  dataValue = signal(0);
 
-  // Создаём поток событий движения мыши
-  stream$: Observable<MouseEvent> = fromEvent<MouseEvent>(document, 'mousemove');
-
-  // Объект для хранения координат мыши
-  coordinates: { x: number; y: number } | null = null;
-
-  public startStream(): void {
-    if (!this.subscription) { // Предотвращаем множественные подписки
-      this.subscription = this.stream$.subscribe({
-        next: (event) => {
-          this.coordinates = { x: event.clientX, y: event.clientY };
-          console.log(`Координаты мыши: (${event.clientX}, ${event.clientY})`);
-        },
-        error: (err) => console.error('Ошибка потока:', err),
-        complete: () => {
-          console.log('Поток завершен');
-          this.subscription = null; // Сбрасываем подписку после завершения
-        }
-      });
-      console.log('Поток запущен');
-    }
-  }
-
-  // Метод для остановки потока (опционально)
-  public stopStream(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-      this.subscription = null;
-      console.log('Поток остановлен');
-    }
-  }
-
-  // Отписываемся от потока при уничтожении компонента
-  ngOnDestroy(): void {
-    this.stopStream();
+  public changeDataValue(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const value = Number(input.value);
+    this.dataValue.set(value);
   }
 }
