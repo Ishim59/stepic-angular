5be86160110a6f4dfde26689f4f6d158db46aea2
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-10 20:36:18 +0300

    An example custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,15 @@ import {
   TitleCasePipe,
   UpperCasePipe
 } from "@angular/common";
+import {ReversePipe} from "./pipe/pipe-reverse";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, DatePipe, UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, DecimalPipe, CurrencyPipe, PercentPipe, JsonPipe],
+  imports: [RouterOutlet, FormsModule, DatePipe, UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, DecimalPipe, CurrencyPipe, PercentPipe, JsonPipe, ReversePipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public text: string = 'Пример текста для демонстрации пайпов';
-  public dateValue: Date = new Date();
-  public amount: number = 1234.567;
-  public percentage: number = 0.789;
-  public items = ['Angular', 'React', 'Vue', 'Svelte'];
-  public longText: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  Suspendisse ultrices, urna a cursus pulvinar, lectus lectus tincidunt nulla,
-  eget tristique magna felis id libero.`;
-
-  public user = {
-    firstName: 'Иван',
-    lastName: 'Иванов',
-    age: 30
-  };
 }
