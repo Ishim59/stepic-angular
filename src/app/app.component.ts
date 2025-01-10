054d5f91c5f9c2f306f59a21f28293df00177f26
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-10 16:18:06 +0300

    An example of how oViewChild works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,19 @@
-import {Component} from '@angular/core';
+import {Component,  ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
-import { AsyncPipe, JsonPipe, NgSwitch, NgSwitchCase, NgSwitchDefault } from "@angular/common";
 import {ChildrenComponent} from "./children/children.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, JsonPipe, NgSwitch, NgSwitchCase, NgSwitchDefault, AsyncPipe, ChildrenComponent],
+  imports: [RouterOutlet, FormsModule,  ChildrenComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    parentString: string = "Строка которая изначально находилась в родителе."
+  @ViewChild(ChildrenComponent, { static: false }) childComponent!: ChildrenComponent;
 
-  onChildClicked($event: any) {
-    this.parentString = $event;
+  triggerChildrenMethod() {
+    this.childComponent.sayHello();
   }
-
-  public onChildrenClickedNewSyntax($event: string) {
-    this.parentString = $event;
-  };
 }
