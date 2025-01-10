ca0b7d6328cc3270c5fdde951ee71bae1e49fd29
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-10 19:57:10 +0300

    An example hostBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
-import {Component, ElementRef, QueryList, Renderer2, ViewChildren} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
-import {ChildrenComponent} from "./children/children.component";
+import {RunAwayDirective} from "./drective /runAwayDirective";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ChildrenComponent],
+  imports: [RouterOutlet, FormsModule, RunAwayDirective],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChildren(ChildrenComponent, { read: ElementRef }) itemsComponents!: QueryList<ElementRef>;
-  constructor(private renderer: Renderer2) {}
 
-  mockList: string[] = ['Первый элеиент', 'Второй элеиент', 'Третий элемент']
-
-  setColorChildren() {
-    this.itemsComponents.forEach((child: ElementRef) => {
-      this.renderer.setStyle(child.nativeElement.querySelector('.children'), 'background-color', 'red');
-    });
-  }
 }
