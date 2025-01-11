170ef964ca8b77f5ea0a281e40717cc44f1c130b
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-12 02:15:14 +0300

    An example ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
@@ -10,32 +10,10 @@ import { CommonModule } from "@angular/common";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  deleteElementNumber: number = 0;
-  content1 = 'Содержимое content 1';
-  content2 = 'Содержимое content 2';
-  content3 = 'Содержимое content 3';
+export class AppComponent {
+  isActive:boolean = false;
 
-  isVisible = true;
-  templates: TemplateRef<any>[] = [];
-
-  @ViewChild('template1') template1!: TemplateRef<any>;
-  @ViewChild('template2') template2!: TemplateRef<any>;
-  @ViewChild('template3') template3!: TemplateRef<any>;
-
-  ngAfterViewInit() {
-    this.templates = [this.template1, this.template2, this.template3];
-  };
-
-  public deleteElement(): void {
-    if(this.deleteElementNumber > 0 && this.deleteElementNumber < 4) {
-      this.templates.splice(this.deleteElementNumber - 1, 1);
-    } else {
-      alert("Число выходит за рамки от 1 до 3")
-    }
-  };
-
-  changeVisible() {
-    this.isVisible = !this.isVisible;
+  changeActive() {
+    this.isActive = !this.isActive;
   };
 }
