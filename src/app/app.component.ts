46a75d093dd5202f3888daecdcee00be27f754ff
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-12 19:38:51 +0300

    An example template form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,19 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, NgForm} from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import {FirstComponent} from "./first/first.component";
-import {SecondComponent} from "./second/second.component";
-import {TotalCounterService} from "./service/sharedService";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, FirstComponent, SecondComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private totalCounterService: TotalCounterService) {
-  };
-
-  public getTotalCounter(): number{
-    return this.totalCounterService.getTotalCounter();
+  onSubmit(myForm: NgForm) {
+    console.log(`Вы ввели имя ${myForm.value.firstField}, а так же email ${myForm.value.email}`);
+    alert(`Вы ввели имя ${myForm.value.firstField}, а так же email ${myForm.value.email}`)
   };
 }
