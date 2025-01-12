5b2d7f51e1d17473b6937fe8ce2ca117f6953878
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-12 13:47:13 +0300

    An example OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import {ChildComponent} from "./child/child.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, ChildComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  item:number = 0
+export class AppComponent implements OnInit {
+  constructor() {
+    alert('Сработал конструктор')
+  };
+
+  ngOnInit() {
+    alert('Сработал метод OnInit')
+  };
 }
