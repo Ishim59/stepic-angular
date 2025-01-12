6106e119d4047dc4bc0f8f46604b21ec9d185b78
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-12 17:14:52 +0300

    An example ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import {ChildComponent} from "./child/child.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, ChildComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isActive: boolean = false;
 
+  public changeActive(): void {
+    this.isActive = !this.isActive;
+  };
 }
