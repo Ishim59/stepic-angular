ae434e5cb6ac61bfe62416a45e0175d777b46baf
i-m-unicorn <max.dev.doc@gmail.com>
2025-01-23 13:22:52 +0300

    example of using basic routes

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TotalCounterService} from "../../service/sharedService";
-import {Router, RouterLink} from "@angular/router";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
